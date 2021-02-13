1c41da4541b67f6a48047c962100e189dcc8f2e5
Saahil Claypool <saahil@claypools.org>
2021-02-13 13:50:46 -0500

    Add reactive component

---
+++ SearchStory.App/Services/AppState.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using SearchStory.App.Shared.Components;
+
+namespace SearchStory.App.Services
+{
+    public class StateContainer
+    {
+        public AppState Value { get; set; }
+        public event Action OnChange;
+
+        public StateContainer() : this(new()) { }
+        public StateContainer(AppState initialState)
+        {
+            Value = initialState;
+        }
+
+        public void Update(Action update)
+        {
+            update();
+            OnChange?.Invoke();
+        }
+    }
+
+    public class AppState
+    {
+        public string Test { get; set; }
+    }
+}

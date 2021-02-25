cd99510aa2e9fb9f1e1fe5551bf6d8c469bf1c4e
Saahil Claypool <saahil@claypools.org>
2021-02-24 21:33:33 -0500

    back to dotnet 5

--- docker_watch.sh
+++ docker_watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose build && docker-compose run web dotnet watch run -r linux-x64 --urls "http://*:5000;https://*:5001"
+docker-compose build && docker-compose run --service-ports web dotnet watch run -r linux-x64 --urls "http://*:5000;https://*:5001"
---
+++ watch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet watch --project ./SearchStory.App/ run -r linux-x64

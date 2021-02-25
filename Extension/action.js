e84c7b4bedcbddc019b2c2daa79fb0f449afd101
Saahil Claypool <saahil@claypools.org>
2021-02-24 22:41:10 -0500

    Extension shows feedback

--- Extension/action.js
+++ Extension/action.js
@@ -23,5 +23,25 @@ if (typeof Readability !== 'undefined') {
             },
             referrerPolicy: 'no-referrer',
             body: payload,
+        }).then(() => 
+        {
+            let id = "search_story_popup";
+            let popup = document.getElementById(id) || document.createElement("div", { id })
+            popup.id = id;
+            try {
+                document.body.removeChild(popup)
+            } catch {
+                console.log('new element')
+            }
+            // clear
+            while(popup.firstChild) popup.removeChild(popup.firstChild);
+            let rounded = "border-radius: .5rem;"
+            let shadow = "--tw-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04); box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);"
+            let center = "display: grid; place-items: center; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); height: 10ch; width: 40ch; text-align: center;"
+            let blue = "background-color: lightblue;";
+            popup.style = rounded + shadow + center + blue
+            popup.appendChild(document.createTextNode("successfully added to Search Story!"));
+            document.body.appendChild(popup)
+            window.setTimeout(() => document.body.removeChild(popup), 1000);
         })
 }
